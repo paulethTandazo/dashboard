639b9444441512848fc17073a54caf2e71ef30b6
paulethTandazo <117050687+paulethTandazo@users.noreply.github.com>
2024-12-01 23:28:11 -0500

    agregando cambios

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Grid from '@mui/material/Grid2' 
+import Grid from '@mui/material/Grid2'
+import IndicatorWeather from './components/IndicatorWeather';
+import TableWeather from './components/TableWeather';
+import ControlWeather from './components/ControlWeather';
+import LineChartWeather from './components/LineChartWeather';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,20 +12,38 @@ function App() {
   return (
     <Grid container spacing={5}>
 
-    {/* Indicadores */}
-    <Grid size={{xs: 12, xl: 3}}>Elemento: Indicador 1</Grid>
-    <Grid size={{xs: 12, xl: 3}}>Elemento: Indicador 2</Grid>
-    <Grid size={{xs: 12, xl: 3}}>Elemento: Indicador 3</Grid>
-    <Grid size={{xs: 12, xl: 3}}>Elemento: Indicador 4</Grid>
-   
-    {/* Tabla */}
-    <Grid size={{xs: 12, xl: 8}}>Elemento: Tabla</Grid>
-   
-    {/* Gráfico */}
-    <Grid size={{xs: 12, xl: 4}}>Elemento: Gráfico 1</Grid>
+      {/* Indicadores */}
+      <Grid size={{ xs: 12, xl: 3 }}>
+        <IndicatorWeather title={'Indicator 1'} subtitle={'Unidad 1'} value={"1.23"} />
+      </Grid>
+      <Grid size={{ xs: 12, xl: 3 }}>
+        <IndicatorWeather title={'Indicator 2'} subtitle={'Unidad 2'} value={"3.12"} />
+      </Grid>
+      <Grid size={{ xs: 12, xl: 3 }}>
+        <IndicatorWeather title={'Indicator 3'} subtitle={'Unidad 3'} value={"2.31"} />
+      </Grid>
+      <Grid size={{ xs: 12, xl: 3 }}>
+        <IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"} />
+      </Grid>
 
-</Grid>
-)
+      {/* Tabla */}
+      <Grid size={{ xs: 12, xl: 8 }}>Elemento: Tabla</Grid>
+      {/* Grid Anidado */}
+      <Grid container spacing={2}>
+        <Grid size={{ xs: 12, xl: 3 }}>
+          <ControlWeather />
+        </Grid>
+        <Grid size={{ xs: 12, xl: 9 }}>
+          <TableWeather />
+        </Grid>
+      </Grid>
+      {/* Gráfico */}
+      <Grid size={{ xs: 12, xl: 4 }}>
+        <LineChartWeather />
+      </Grid>
+
+    </Grid >
+  )
 }
 
 export default App
